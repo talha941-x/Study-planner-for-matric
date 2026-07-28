fedf0a0581df119eccdbe9c4b91de5b39958d42c
talha941-x <t93180413@gmail.com>
2026-07-28 10:48:36 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,22 @@
-const CACHE = "planner-v1";
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["./", "./index.html", "./manifest.json"])));
+const CACHE_NAME = "study-planner-v1";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
+  );
 });
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
